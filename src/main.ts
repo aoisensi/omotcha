bd337736a247fca0364a4e50ab9f073a9c45d333
aoisensi <me@aoisensi.info>
2024-07-30 15:34:40 +0900

    0.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,105 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import TMI from 'tmi.js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const elementName = document.getElementById('name')!
+const elementMessage = document.getElementById('message')!
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const audioContext = new window.AudioContext();
+
+const response = await fetch('SYS_text.ogg');
+const arrayBuffer = await response.arrayBuffer();
+const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+class Message {
+  constructor(public name: string, public message: string) {}
+}
+
+const stack: Message[] = [];
+
+var ppp_id: number;
+var hdd_id: number;
+var waiting = false;
+
+const client = new TMI.Client({
+  // options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true
+  },
+  identity: {
+    username: 'justinfan143',
+  },
+  channels: [ new URLSearchParams(window.location.search).get('channel') || 'twitchpresents' ]
+});
+client.connect().catch(console.error);
+client.on('message', (_, tags, message, __) => {
+  pushMessage(new Message(tags['display-name'] || 'Something', message));
+});
+
+function pushMessage(message: Message) {
+  //if stack is empty
+  if (stack.length === 0 || waiting) {
+    show(message);
+  } else {
+    stack.push(message);
+  }
+}
+
+function show(message: Message) {
+  waiting = false;
+  console.log(message.name, message.message);
+  elementName.innerText = message.name;
+  elementMessage.innerText = '';
+  show_box();
+  clearInterval(hdd_id);
+  /*
+  setTimeout(() => {
+    const new_message = stack.shift();
+    if(new_message) show(new_message);
+  }, 5000);
+  */
+  var i = 0;
+  ppp_id = setInterval(() => {
+    i++;
+    elementMessage.innerText = message.message.slice(0, i);
+    play_text();
+    if (i === message.message.length) {
+      clearInterval(ppp_id);
+      setTimeout(() => {
+        const new_message = stack.shift();
+        if(new_message){
+          show(new_message)
+        } else {
+          waiting = true;
+        }
+      }, 3000);
+      hdd_id = setTimeout(() => {
+        console.log('hide')
+        hide_box();
+      }, 14300);
+    }
+  }, 66);
+}
+
+function show_box() {
+  document.querySelectorAll('.box').forEach((e) => {
+    e.classList.remove('hidden');
+  });
+}
+
+function hide_box() {
+  document.querySelectorAll('.box').forEach((e) => {
+    e.classList.add('hidden');
+  });
+}
+
+function play_text() {
+  const randomPitch = 0.9 + Math.random() * 0.2;
+  const audioSource = audioContext.createBufferSource();
+  const gain = audioContext.createGain();
+  gain.gain.value = 0.1;
+  audioSource.buffer = audioBuffer;
+  audioSource.playbackRate.value = randomPitch;
+  audioSource.playbackRate
+  audioSource.connect(gain);
+  gain.connect(audioContext.destination);
+  audioSource.start(0);
+}
